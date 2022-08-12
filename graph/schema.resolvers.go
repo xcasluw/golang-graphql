3f1606515117d1fa8c654dba686efc50a58e2847
Lucas Genari <oligena@hotmail.com>
2022-08-12 09:24:11 -0300

    adding queries

---
+++ graph/schema.resolvers.go
@@ -0,0 +1,50 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+	"xcasluw/golang-graphql/graph/generated"
+	"xcasluw/golang-graphql/graph/model"
+)
+
+// CreateCategory is the resolver for the createCategory field.
+func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategoryInput) (*model.Category, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// CreateCourse is the resolver for the createCourse field.
+func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCourseInput) (*model.Course, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// CreateChapter is the resolver for the createChapter field.
+func (r *mutationResolver) CreateChapter(ctx context.Context, input model.NewChapterInput) (*model.Chapter, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Categories is the resolver for the categories field.
+func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
+	return r.Resolver.Categories, nil
+}
+
+// Courses is the resolver for the courses field.
+func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Chapters is the resolver for the chapters field.
+func (r *queryResolver) Chapters(ctx context.Context) ([]*model.Chapter, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
